4df05bd18af7f4f47369bd833240eb1c70e0c1aa
Pallavi2911 <>
2023-02-13 17:57:31 +0900

    till 129 navigating with router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -11,6 +10,15 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+//import is Routes
+import { Routes, RouterModule } from '@angular/router';
+
+//to define all the routes of our application
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent },
+];
 
 @NgModule({
   declarations: [
@@ -20,13 +28,16 @@ import { ServersService } from './servers/servers.service';
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    //to register routes in our app or in other words to add routing functionality to our app
+    //forRoot allows us to register some routes for our main application
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ServersService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
