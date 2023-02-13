06ec0ed8d88a79d3f8153cdb0c7e3edee9bcb2e1
Pallavi2911 <>
2023-02-13 22:33:39 +0900

    till 134 fetch route params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-  }
+    //approach to retrieve route params when you know your component will always be created when it is reached
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name'],
+    };
 
+    //approach to retrieve route params when you know your component can be reloaded while being on the same component
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
+  }
 }
