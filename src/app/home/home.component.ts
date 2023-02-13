91a8d937913597d00bd3696b71712665e291b664
Pallavi2911 <>
2023-02-13 18:52:40 +0900

    till 131 programmatic navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  //inject router
+  constructor(
+    private router: Router,
+    //ActivatedRoute loads the currently active route
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLoadServes() {
+    //navigate to servers component
+    //navigate using absolute path
+    // this.router.navigate(['/servers']);
+    //navigate using relative path
+    //relativeTo defines relative to which route this link should be loaded . When not defined it is root domain by default
+    this.router.navigate(['servers'], { relativeTo: this.route });
   }
-
 }
