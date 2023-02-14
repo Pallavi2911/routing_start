e35443d8ac92c3af6821ca3fa5c49d7a87763f5a
Pallavi2911 <>
2023-02-14 17:55:46 +0900

    147 protecting routes with canActivate

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+//angular executes this code before route is loaded
+//So CanActivate can run both asynchronously returning an observable or a promise or synchronously
+//because you might have some guards which execute some code which runs completely on the client,
+//therefore it runs synchronously.
+//add injectable to be able to reach out to other service
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  // implement this method always
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+}
---
+++ src/app/auth.service.ts
@@ -0,0 +1,21 @@
+export class AuthService {
+  //check authentication state
+  loggedIn = false;
+
+  isAuthenticated() {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+    return promise;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
