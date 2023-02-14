36ec47ceadbb02306aad72d2ef8d09caeb3392d8
Pallavi2911 <>
2023-02-14 14:38:05 +0900

    144 outsourcing route configuration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
+
+//to define all the routes of our application
+//children indicate child routes
+//routes get parsed from top to bottom
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id/:name', component: UserComponent }],
+  },
+
+  {
+    path: 'servers',
+    component: ServersComponent,
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ],
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  //If you don't want to specify a component to load ,use redirectTo by specifying a path
+  // ** is a wildcard route, it should be last one always
+  { path: '**', redirectTo: '/not-found' },
+];
+
+@NgModule({
+  //no need to add declarations here because these are already declared in app module
+  imports: [
+    //to register routes in our app or in other words to add routing functionality to our app
+    //forRoot allows us to register some routes for our main application
+    RouterModule.forRoot(appRoutes),
+  ],
+  //exports help to add this module to the imports of another module
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,34 +10,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-//import is Routes
-import { Routes, RouterModule } from '@angular/router';
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-//to define all the routes of our application
-//children indicate child routes
-//routes get parsed from top to bottom
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [{ path: ':id/:name', component: UserComponent }],
-  },
-
-  {
-    path: 'servers',
-    component: ServersComponent,
-    children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-    ],
-  },
-  { path: 'not-found', component: PageNotFoundComponent },
-  //If you don't want to specify a component to load ,use redirectTo by specifying a path
-  // ** is a wildcard route, it should be last one always
-  { path: '**', redirectTo: '/not-found' },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -50,13 +25,7 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    //to register routes in our app or in other words to add routing functionality to our app
-    //forRoot allows us to register some routes for our main application
-    RouterModule.forRoot(appRoutes),
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
